440905d5056946fe72e64d980186dfabcfd08820
Angelo D S Alvarez <angelodaniel1221@gmail.com>
2024-04-08 20:09:51 -0300

    Processadores agora ficam em uma tabela e utiliza função atraves de onclick para adicionar na tabela final

--- index.php
+++ index.php
@@ -4,14 +4,13 @@
     $connection = mysqli_connect("localhost:3306", "root", "root", "fazorcamento") or die ("Error: " . mysqli_error($connection));
 
     //query para pegar os alimentos do bd
-    $query = "  SELECT * FROM processador
+    $queryProcessador = "  SELECT * FROM processador
                 ORDER BY nome; ";
 
     //executa a query
-    $resultado = mysqli_query($connection, $query) or die ("Erro ao selecionar " . mysqli_error($connection));
+    $resultado = mysqli_query($connection, $queryProcessador) or die ("Erro ao selecionar " . mysqli_error($connection));
 
     //array para armazenar o resultado
-
     $arrayProcessador = array();
     
     //passa o resultado para o array
@@ -26,8 +25,30 @@
     fwrite($jsonAlimentos, json_encode($arrayAlimentos));
     fclose($jsonAlimentos);
 
-    //encaminha para a pagina de adicionar a dieta
-    //header ('Location: ./adicionar-dieta.php');
+
+
+    //query para pegar os alimentos do bd
+    $queryPlacaMae = "  SELECT * FROM placa_mae
+                ORDER BY nome; ";
+
+    //executa a query
+    $resultado = mysqli_query($connection, $queryPlacaMae) or die ("Erro ao selecionar " . mysqli_error($connection));
+
+    //array para armazenar o resultado
+    $arrayProcessador = array();
+    
+    //passa o resultado para o array
+    while($row = mysqli_fetch_assoc($resultado)){
+        $arrayAlimentos[] = $row;
+    }
+
+    //caminho do JSON
+    $jsonAlimentos = fopen('./json/placa-mae.json', 'w');
+
+    //escreve no JSON e utiliza a função JSON_encode
+    fwrite($jsonAlimentos, json_encode($arrayAlimentos));
+    fclose($jsonAlimentos);
+
 ?>
 
 
@@ -37,7 +58,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Teste</title>
     <script type="text/javascript" src="./script.js"></script>
 </head>
 <body>
@@ -48,9 +69,47 @@
             <script>atualizaSelect()</script>
         </select>
 
+        
+
         <input id="valor-processador"  disabled>
+        
+        <table>
+            <thead>
+                <th>Nome</th>
+                <th>Valor</th>
+            </thead>
+
+            <tbody class="Processador" id="tabela-processadores">
+                <script>atualizaTabela()</script>
+            </tbody>
+        </table>
+        
+        <table>
+            <thead>
+                <th>Nome</th>
+                <th>ChipSet</th>
+                <th>Soquete</th>
+                <th>Valor</th>
+            </thead>
+
+            <tbody class="Placa-mae" id="tabela-processadores">
+                <script>atualizaTabela()</script>
+            </tbody>
+        </table>
 
 
+
+        <table >
+            <thead>
+                <th>Componente</th>
+                <th>Nome</th>
+                <th>Valor</th>
+            </thead>
+
+            <tbody id="tabela-componentes">
+                
+            </tbody>
+        </table>
 
 
 
--- script.js
+++ script.js
@@ -18,6 +18,31 @@ function atualizaSelect(){
     })
 }
 
+function atualizaTabela() {
+    
+    //usa o querySelector para pegar o select dos alimentos
+    var tabelaProcessadores = document.querySelector("#tabela-processadores");
+    
+    //pega o JSON de alimentos e transforma a resposta em "response"
+    fetch("./json/processadores.json").then((response) => {
+        //converte a resposta em JSON e após for convertido (.then) possuo os "alimentos"
+        response.json().then((processadores) =>{
+            
+            //map pelo JSON de alimentos para percorrer por todos
+            tabelaProcessadores.innerHTML = ``;
+            processadores.map((processador) => {
+                //innerHTML na tabelaProcessadores, para inserir o processador na tabela
+                //onclick passando o id do processador e o nome do componente para colocar na tabela final dos componentes 
+                tabelaProcessadores.innerHTML +=    `
+                <tr onclick="alteraProcessador(${processador.id_processador},'${tabelaProcessadores.className}')">
+                    <td > ${processador.nome} </td>
+                    <td > ${processador.preco} </td>
+                </tr>
+                `;
+            })
+        })
+    })
+}
 
 function alteraInput() {
     let valorProcessador = document.querySelector("#valor-processador");
@@ -36,4 +61,34 @@ function alteraInput() {
         })
     })
 
+}
+
+
+function alteraProcessador(id_processador, componente) {
+    let tabelaComponentes = document.querySelector("#tabela-componentes");
+
+    //pega o JSON de alimentos e transforma a resposta em "response"
+    fetch("./json/processadores.json").then((response) => {
+        //converte a resposta em JSON e após for convertido (.then) possuo os "alimentos"
+        response.json().then((processadores) =>{
+            
+            //map pelo JSON de alimentos para percorrer por todos
+            processadores.map((processador) => {
+                if (id_processador == processador.id_processador){
+                    let trComponente = document.getElementById(componente);
+                    if(trComponente != null) {
+                        trComponente.remove();
+                    }
+                    //innerHTML no "selectAlimentos" para inserir os alimentos no select
+                    tabelaComponentes.innerHTML +=    `
+                    <tr id="${componente}">
+                        <td > ${componente} </td>
+                        <td > ${processador.nome} </td>
+                        <td > R$ ${processador.preco} </td>
+                    </tr>
+                    `;
+                } 
+            })
+        })
+    })
 }
