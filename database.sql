025c6e2e1fc91d235412a5f022f9e748489b24da
Angelo D S Alvarez <angelodaniel1221@gmail.com>
2024-04-08 20:06:11 -0300

    banco de dados atual

---
+++ database.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE fazorcamento;
+USE fazorcamento;
+
+CREATE TABLE processador(
+	id_processador INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200),
+    preco FLOAT
+);
+
+
+insert into processador VALUES
+(null, "i5 6400", 200),
+(null, "i7 2700", 200);
+
+
+select * from processador;
+
+CREATE TABLE placa_mae(
+	id_placamae INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    chipset VARCHAR(10),
+    soquete VARCHAR(20),
+    valor FLOAT
+);
+
+INSERT INTO placa_mae VALUES
+(null, "Duex", "h61", "lga1155", 120),
+(null, "Blucase", "h110", "lga1151", 220);
+
+
+select * from placa_mae;
